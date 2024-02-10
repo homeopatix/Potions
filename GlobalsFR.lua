4faed3436eb0e13cb6146d55a9816c23fd9724b6
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-10 15:35:48 +0100

    Create GlobalsFR.lua

---
+++ GlobalsFR.lua
@@ -0,0 +1,52 @@
+------------------------------------------------------------------------------------------
+-- GlobalFR file
+-- Written by Homeopatix
+-- 17 avril 2021
+-- updated 28 decembre 2023
+------------------------------------------------------------------------------------------
+_G.T = {};
+	------------------------------------------------------------------------------------------
+	-- Francais --
+	------------------------------------------------------------------------------------------
+	T[ "PluginName" ] = "Potions";
+	T[ "PluginText" ] = "Par Homeopatix";
+	T[ "Loaded" ] = "charg\195\169";
+	T[ "Unactivated" ] = "D\195\169sactiv\195\169";
+	T[ "PluginOptionsWindowShow" ] = "Affiche la fen\195\168tre d'options";
+	------------------------------------------------------------------------------------------
+	-- help --
+	------------------------------------------------------------------------------------------
+	T[ "PluginHelp1" ] = "\n\nListe des commandes:\n";
+	T[ "PluginHelp2" ] = "/Pot show - affiche la fen\195\168tre.\n";
+	T[ "PluginHelp3" ] = "/Pot hide - cache la fen\195\168tre.\n";
+	T[ "PluginHelp4" ] = "/Pot options - affiche la fen\195\168tre d'options.\n";
+	T[ "PluginHelp5" ] = "/Pot esc - active ou desactive la touche escape.\n";
+	T[ "PluginHelp6" ] = "/Pot alt - Active ou d\195\169sactive la touche alt pour le d\195\169placement de l'icon.\n";
+	T[ "PluginHelp7" ] = "/Pot clear - efface tous les raccourcis.\n";
+	T[ "PluginHelp8" ] = "/Pot lock pour verrouill\195\169 ou d\195\169verrouill\195\169 les icones\n";
+	T[ "PluginHelp9" ] = "/Pot toggle - Affiche ou cache la fen\195\168tre\n";
+	T[ "PluginHelp10" ] = "/Pot always - Affiche toujours la fen\195\168tre\n\n";
+	T[ "PluginHelp11" ] = "Clique droit sur l'icon pour afficher la fen\195\168tre d'options";
+	------------------------------------------------------------------------------------------
+	-- command text  --
+	------------------------------------------------------------------------------------------
+	T[ "PluginWindowShow" ] = "Affiche la fen\195\168tre.";
+	T[ "PluginWindowHide" ] = "Cache la fen\195\168tre.";
+	T[ "PluginWindowClear" ] = "Efface tous les raccourcis.";
+	T[ "PluginLocked" ] = "Icones verrouill\195\169es";
+	T[ "PluginUnlocked" ] = "Icones d\195\169verouill\195\169es";
+	T[ "PluginWindowsAlwaysShowOn" ] = "Afficher toujours la fen\195\168tre ON";
+	T[ "PluginWindowsAlwaysShowOff" ] = "Afficher toujours la fen\195\168tre OFF";
+	T[ "PluginEscEnable" ] = "Touche Escape activ\195\169";
+	T[ "PluginEscDesable" ] = "Touche Escape desactiv\195\169";
+	T[ "PluginAltEnable" ] = "Touche Alt activ\195\169";
+	T[ "PluginAltDesable" ] = "Touche Alt desactiv\195\169";
+	------------------------------------------------------------------------------------------
+	-- Options --
+	------------------------------------------------------------------------------------------
+	T[ "PluginOptionsText" ] = "Potions Options";
+	T[ "PluginOption1" ] = "Nombre de lignes";
+	T[ "PluginOption2" ] = "Nombre d'emplacements par ligne";
+	T[ "PluginOption3" ] = " Afficher la bordure de la fen\195\168tre";
+	T[ "PluginOption4" ] = " Afficher toujours la fen\195\168tre";
+	T[ "PluginOptionValidate" ] = "Valider les changements";
