d73c9f78155b421620c8b0e8d09005b6e6ef1858
Daniel D <spawn900@users.noreply.github.com>
2024-02-13 11:40:45 +0100

    Update GlobalsDE.lua - Potions
    Updated the german translation

--- GlobalsDE.lua
+++ GlobalsDE.lua
@@ -11,32 +11,32 @@ _G.T = {};
 	T[ "PluginName" ] = "Potions";
 	T[ "PluginText" ] = "Von Homeopatix";
 	T[ "Loaded" ] = "Geladen";
-	T[ "Unactivated" ] = "Deaktiviert";
-	T[ "PluginOptionsWindowShow" ] = "Zeigt das Optionsfenster an";
+	T[ "Unactivated" ] = "Beendet";
+	T[ "PluginOptionsWindowShow" ] = "Optionen werden angezeigt";
 	------------------------------------------------------------------------------------------
 	-- help --
 	------------------------------------------------------------------------------------------
 	T[ "PluginHelp1" ] = "\n\nListe der Befehle:\n";
-	T[ "PluginHelp2" ] = "/Pot show - Schaufenster.\n";
-	T[ "PluginHelp3" ] = "/Pot hide - verstecke das Fenster.\n";
-	T[ "PluginHelp4" ] = "/Pot options - Zeigen Sie das Optionsfenster an.\n";
-	T[ "PluginHelp5" ] = "/Pot esc - Aktivieren oder deaktivieren Sie die Escape-Taste.\n";
-	T[ "PluginHelp6" ] = "/Pot clear - L\195\182schen Sie alle Verkn\195\188pfungen.\n";
-	T[ "PluginHelp7" ] = "/Pot alt - ALT-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
-	T[ "PluginHelp8" ] = "/Pot lock um die Symbole zu sperren oder zu entsperren\n";
-	T[ "PluginHelp9" ] = "/Pot toggle - Fenster ein-oder ausblenden\n";
-	T[ "PluginHelp10" ] = "/Pot always - Zeigt immer das Fenster an\n\n";
-	T[ "PluginHelp11" ] = "Klicken Sie mit der rechten Maustaste auf das Symbol, um das Optionsfeld anzuzeigen";
+	T[ "PluginHelp2" ] = "/pot show - Zeigt das Fenster an.\n";
+	T[ "PluginHelp3" ] = "/pot hide - Blendet das Fenster aus.\n";
+	T[ "PluginHelp4" ] = "/pot options - Zeigt die Optionen an.\n";
+	T[ "PluginHelp5" ] = "/pot esc - Aktiviert/Deaktiviert die Escape-Taste, um das Fenster zu schlie\195\159en.\n";
+	T[ "PluginHelp6" ] = "/pot clear - L\195\182scht alle Verkn\195\188pfungen.\n";
+	T[ "PluginHelp7" ] = "/pot alt - Alt-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
+	T[ "PluginHelp8" ] = "/pot lock Sperrt/entsperrt die Verkn\195\188pfungen.\n";
+	T[ "PluginHelp9" ] = "/pot toggle - Fenster anzeigen/ausblenden.\n";
+	T[ "PluginHelp10" ] = "/pot always - Zeigt das Fenster immer an.\n\n";
+	T[ "PluginHelp11" ] = "Klicke mit der rechten Maustaste auf das Symbol, um die Optionen anzuzeigen.";
 	------------------------------------------------------------------------------------------
 	-- command text  --
 	------------------------------------------------------------------------------------------
-	T[ "PluginWindowShow" ] = "Schaufenster.";
-	T[ "PluginWindowHide" ] = "verstecke das Fenster.";
-	T[ "PluginWindowClear" ] = "L\195\182schen Sie alle Verkn\195\188pfungen.";
-	T[ "PluginLocked" ] = "Gesperrte Symbole";
-	T[ "PluginUnlocked" ] = "Symbole freigeschaltet";
-	T[ "PluginWindowsAlwaysShowOn" ] = "Fenster immer eingeschaltet anzeigen";
-	T[ "PluginWindowsAlwaysShowOff" ] = "Fenster immer AUS anzeigen";
+	T[ "PluginWindowShow" ] = "Fenster wird angezeigt";
+	T[ "PluginWindowHide" ] = "Fenster wird ausgeblendet";
+	T[ "PluginWindowClear" ] = "Alle Verkn\195\188pfungen gel\195\182scht!";
+	T[ "PluginLocked" ] = "Verkn\195\188pfungen gesperrt";
+	T[ "PluginUnlocked" ] = "Verkn\195\188pfungen entsperrt";
+	T[ "PluginWindowsAlwaysShowOn" ] = "Fenster wird immer angezeigt";
+	T[ "PluginWindowsAlwaysShowOff" ] = "Fenster wird immmer ausgeblendet";
 	T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
 	T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
 	T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
@@ -44,9 +44,9 @@ _G.T = {};
 	------------------------------------------------------------------------------------------
 	-- Options --
 	------------------------------------------------------------------------------------------
-	T[ "PluginOptionsText" ] = "Optionsfenster";
+	T[ "PluginOptionsText" ] = "Potions Optionen";
 	T[ "PluginOption1" ] = "Anzahl der Zeilen";
-	T[ "PluginOption2" ] = "Anzahl der Steckpl\195\164tze pro Zeile";
-	T[ "PluginOption3" ] = " Zeigen Sie den Fensterrand an";
-	T[ "PluginOption4" ] = " Fenster immer eingeschaltet anzeigen";
-	T[ "PluginOptionValidate" ] = "\195\132nderungen validieren";
+	T[ "PluginOption2" ] = "Anzahl der Spalten";
+	T[ "PluginOption3" ] = " Fensterrand anzeigen";
+	T[ "PluginOption4" ] = " Fenster dauerhaft anzeigen";
+	T[ "PluginOptionValidate" ] = "Speichern";
